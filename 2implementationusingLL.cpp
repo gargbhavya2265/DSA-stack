1fe40c4287844ea73eab191629c29883cbfb05e5
gargbhavya2265 <bhavyagarg2265@gmail.com>
2025-08-07 15:33:33 +0530

    Add files via upload

---
+++ 2implementationusingLL.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class Stack{
+    public:
+        Node* top;
+        int size;
+    Stack(){
+       top=NULL;
+       size=0;
+    }
+
+    void push(int value){
+        Node* temp=new Node(top);
+        temp->next=top;
+        top=
+    }
+};
+int main(){
+
+}
