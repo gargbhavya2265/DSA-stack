c17406a47c1c36de285d9adadcf83afc6907c268
gargbhavya2265 <bhavyagarg2265@gmail.com>
2025-08-07 15:43:00 +0530

    Add files via upload

---
+++ 12getMInAtPop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of elements"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"enter the list of elements "<<endl;
+    for(int i=0;i<n;i++){
+        
+    }
+}
